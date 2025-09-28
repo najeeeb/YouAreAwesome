615e5310e8cbc7fcac18a1fe31a33c8d7b4eec01
najib atfa <atfa.najib@gmail.com>
2025-09-28 14:16:39 +0100

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Atfa on 28/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
