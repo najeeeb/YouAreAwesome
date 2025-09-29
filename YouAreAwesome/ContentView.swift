117d0b6ba06ad2f5d0847e004375642826f614cf
najib atfa <atfa.najib@gmail.com>
2025-09-29 22:38:59 +0100

    Added ' I am a developer '

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,25 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("What is football for you")
+            Text("I am a developer")
                 .font(.title)
-                .fontWeight(.black)
-                .foregroundStyle(.green)
-            HStack{
-                Image(systemName: "figure.american.football")
+                .fontWeight(.heavy)
+                .foregroundStyle(.black)
+                Image(systemName: "play.laptopcomputer")
                     .resizable()
                     .scaledToFit()
                     .foregroundStyle(.blue)
-                Image(systemName: "figure.australian.football")
-                    .resizable()
-                    .scaledToFit()
-                    .foregroundStyle(.indigo)
-                Image(systemName: "figure.indoor.soccer")
-                    .resizable()
-                    .scaledToFit()
-                    .foregroundStyle(.purple)
-            }
-            
             
         }
         .padding()
